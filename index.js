6bea00d16093b9c51ee49bc849eb6cc0c2eb0765
z_i_n <zin.never@gmail.com>
2015-02-09 13:07:06 +0900

    demo3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var cool = require('cool-ascii-faces');
 
 console.log(process.env.PORT);
-app.set('port', (5000));
+app.set('port', (process.env.PORT || 5000));
 //app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
