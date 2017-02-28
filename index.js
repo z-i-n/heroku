7892f34d4e2adc9d0249a1ffc1965521307f7805
ray.choi <hyoungjin.choi@yogiyo.co.kr>
2017-02-28 14:33:49 +0900

    react test

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
+'use strict';
 var express = require('express');
-var app = express();
-var cool = require('cool-ascii-faces');
+var path = require("path");
+var api = require('./server/api/api');
+
+/* Start Dev Server */
+var app     = express();
 
 console.log(process.env.PORT);
-app.set('port', (process.env.PORT || 8000));
-//app.use(express.static(__dirname + '/public'));
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, '..', 'dist')));
 
-app.get('/', function(request, response) {
-  response.send(cool());
+// Always return the main index.html, so react-router render the route in the client
+app.get(/^((?!\/api).)*$/, (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
 });
 
-var pg = require('pg');
+// /^((?!\/api).)*$/
+app.use('/api', api);
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
-    });
-  });
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  // var err = new Error('Not Found');
+  // err.status = 404;
+  //next(err);
+  res.status(404).send('Not found');
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.listen(8000, function () {
+      console.log('Example app listening on port 8000');
 });
