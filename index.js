fed53a333266e565f9a4df9d5f67adb3ae9fd796
ray.choi <hyoungjin.choi@yogiyo.co.kr>
2017-03-07 19:00:04 +0900

    webrtc

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 'use strict';
 var express = require('express');
 var path = require("path");
-var sockets = require('./sockets');
-var config = require('getconfig');
 
 /* Start Dev Server */
 var app     = express();
-var server = app.listen(process.env.PORT || 5000);
-sockets(server, config);
 console.log(process.env.PORT);
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '.', 'dist')));
 
 // Always return the main index.html, so react-router render the route in the client
-app.get('/page', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '.', 'dist', 'index.html'));
 });
 
@@ -28,6 +24,6 @@ app.use(function(req, res, next) {
   res.status(404).send('Not found');
 });
 
-// app.listen(app.get('port'), function() {
-//   console.log("Node app is running at localhost:" + app.get('port'));
-// });
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
